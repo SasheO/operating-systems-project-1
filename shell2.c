0745b57b030aba900571622777dc43a9b70c7b84
Mezisashe Ojuba <mezisashe.ojuba@bison.howard.edu>
2024-10-10 13:49:10 +0000

    implemented pwd

---
+++ shell2.c
@@ -0,0 +1,120 @@
+/*
+    Author: Mezisashe Ojuba
+    Written: 
+*/
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define MAX_COMMAND_LINE_LEN 1024
+#define MAX_COMMAND_LINE_ARGS 128
+
+char prompt[] = "> ";
+char current_dir[MAX_COMMAND_LINE_LEN];
+char delimiters[] = " \t\r\n";
+extern char **environ;
+// Stores the string typed into the command line.
+char command_line[MAX_COMMAND_LINE_LEN];
+char cmd_bak[MAX_COMMAND_LINE_LEN];
+
+// cite trim, ltrim and rtrim from https://stackoverflow.com/questions/656542/trim-a-string-in-c
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+void trim_command_line() // get rid of any white trailing white space and beginning or end
+{
+    char* trimmed_command = rtrim(ltrim(command_line));
+    strcpy(command_line, trimmed_command);
+}
+
+int main() { 
+    // Stores the tokenized command line input.
+    char *arguments[MAX_COMMAND_LINE_ARGS];
+    	
+    while (true) {
+      
+        do{ 
+            // Print the shell prompt.
+            getcwd(current_dir, MAX_COMMAND_LINE_LEN);
+            printf("%s%s", current_dir, prompt);
+            fflush(stdout);
+
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
+            
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);
+            }
+
+            trim_command_line();
+            
+            if (strcmp(command_line,"pwd") == 0){
+                printf("%s\n", current_dir);
+            }
+            // TODO: implement cd. check if cd is the first argument then there's another argument
+ 
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+
+      
+        // If the user input was EOF (ctrl+d), exit the shell.
+        if (feof(stdin)) {
+            printf("\n");
+            fflush(stdout);
+            fflush(stderr);
+            return 0;
+        }
+
+        // TODO:
+        // 
+        
+			  // 0. Modify the prompt to print the current working directory
+			  
+			
+        // 1. Tokenize the command line input (split it on whitespace)
+
+      
+        // 2. Implement Built-In Commands
+      
+    
+        // 3. Create a child process which will execute the command line input
+
+  
+        // 4. The parent process should wait for the child to complete unless its a background process
+      
+      
+        // Hints (put these into Google):
+        // man fork
+        // man execvp
+        // man wait
+        // man strtok
+        // man environ
+        // man signals
+        
+        // Extra Credit
+        // man dup2
+        // man open
+        // man pipes
+    }
+    // This should never be reached.
+    return -1;
+}
